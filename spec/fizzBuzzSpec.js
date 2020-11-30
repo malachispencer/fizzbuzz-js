1e665e2ad05319a3f4b138c5a3b1578a668326e9
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-30 21:11:48 +0000

    Passed test for 15 to return FizzBuzz

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,3 +1,6 @@
+const fizzBuzz = require('../src/fizzBuzz.js');
+const fb = require('../src/fizzBuzz.js');
+
 describe('fizzBuzz', () => {
   it('returns FizzBuzz if number is divisible by 3 and 5', () => {
     expect(fizzBuzz(15)).toEqual('FizzBuzz')
---
+++ src/fizzBuzz.js
@@ -0,0 +1,7 @@
+function fizzBuzz(n) {
+  if (n % 3 === 0 && n % 5 === 0) {
+    return 'FizzBuzz';
+  }
+}
+
+module.exports = fizzBuzz;
